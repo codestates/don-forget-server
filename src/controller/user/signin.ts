96f1cc97fa9b1d40f8ac5362d959152edf67433d
bapbodanbbang <62498196+bapbodanbbang@users.noreply.github.com>
2020-10-13 15:39:58 +0900

    test for social login : kakao

--- src/controller/user/signin.ts
+++ src/controller/user/signin.ts
@@ -9,45 +9,118 @@ import bcrypt from 'bcrypt';
 */
 
 export async function signin (req:Request, res:Response) {
+  
+  //소셜로그인이라면 req.body.socialLogin에 'kakao'나 'naver'기입
+  const { socialLogin, email, password, name } = req.body;
 
-  const { email, password } = req.body;
+  //기존 코드
   const session = req!.session!;
   const hash_password = await User.findOne({
     where: {
       email: email
     }
   })
-  
-  await bcrypt.compare(password, hash_password?.getDataValue('password'), (err, result) => {
-    if(err) {
-      console.log(err);
-      res.status(401).send("Unauthorized")
-    } else {
-      // result = true;
-      (async function findUser() {
-        const user = await User.findOne({
-          where: {
-            email: email
-          }
+
+  //소셜로그인의 경우
+  if(socialLogin === 'kakao' || socialLogin === 'naver'){
+    //패스워드는 자신 이메일 기반으로 만듦
+    const encrypt_password_by_email = await bcrypt.hash(email, 10);
+    //신규 사용자인 경우 회원가입을 먼저 시긴다.
+    if(hash_password === null){
+      async function createUser() {
+        await User.create({
+          name: name,
+          email: email,
+          password: encrypt_password_by_email
         })
-        if(user === null) {
-          res.status(401).send("Unauthorized")
-        } else {
-          console.log("user:", user?.getDataValue('id'))
+        .then(newUser => {
+          console.log("newUser:", newUser?.getDataValue('id'))
           console.log("session:", session)
-          session.userid = user?.getDataValue('id');
-          session.name = user?.getDataValue('name');
-          session.email = user?.getDataValue('email');
-          session.password_answer = user?.getDataValue('password_answer');
+          session.userid = newUser?.getDataValue('id');
+          session.name = newUser?.getDataValue('name');
+          session.email = newUser?.getDataValue('email');
+          session.password_answer = newUser?.getDataValue('password_answer');
           session.save(function () {
-            res.status(200).json({
-              "id": user?.getDataValue('id'),
-              "name": user?.getDataValue('name'),
-              "email": user?.getDataValue('email')
+            res.json({
+              "id": newUser?.getDataValue('id'),
+              "name": newUser?.getDataValue('name'),
+              "email": newUser?.getDataValue('email')
             })
           })
-        }
-      })();
+        })
+      }
+      await createUser();
     }
-  })
+    //로그인 실행
+    await bcrypt.compare(email, hash_password?.getDataValue('password'), (err, result) => {
+      if(err) {
+        console.log(err);
+        res.status(401).send("Unauthorized")
+      } else {
+        // result = true;
+        (async function findUser() {
+          const user = await User.findOne({
+            where: {
+              email: email
+            }
+          })
+          if(user === null) {
+            res.status(401).send("Unauthorized")
+          } else {
+            console.log("user:", user?.getDataValue('id'))
+            console.log("session:", session)
+            session.userid = user?.getDataValue('id');
+            session.name = user?.getDataValue('name');
+            session.email = user?.getDataValue('email');
+            session.password_answer = user?.getDataValue('password_answer');
+            session.save(function () {
+              res.status(200).json({
+                "id": user?.getDataValue('id'),
+                "name": user?.getDataValue('name'),
+                "email": user?.getDataValue('email')
+              })
+            })
+          }
+        })();
+      }
+    })
+  }
+
+  //일반로그인의 경우
+  else{
+    
+    await bcrypt.compare(password, hash_password?.getDataValue('password'), (err, result) => {
+      if(err) {
+        console.log(err);
+        res.status(401).send("Unauthorized")
+      } else {
+        // result = true;
+        (async function findUser() {
+          const user = await User.findOne({
+            where: {
+              email: email
+            }
+          })
+          if(user === null) {
+            res.status(401).send("Unauthorized")
+          } else {
+            console.log("user:", user?.getDataValue('id'))
+            console.log("session:", session)
+            session.userid = user?.getDataValue('id');
+            session.name = user?.getDataValue('name');
+            session.email = user?.getDataValue('email');
+            session.password_answer = user?.getDataValue('password_answer');
+            session.save(function () {
+              res.status(200).json({
+                "id": user?.getDataValue('id'),
+                "name": user?.getDataValue('name'),
+                "email": user?.getDataValue('email')
+              })
+            })
+          }
+        })();
+      }
+    })
+  }
+
 }
