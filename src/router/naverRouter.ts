dfd81ef890a545f0d365b56cfea7eb74b615c512
chaerynny <ryn3777@gmail.com>
2020-10-14 14:37:54 +0900

    [ADD] naver login

---
+++ src/router/naverRouter.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import {Request, Response} from 'express';
+// import { naverLogin } from '../controller/user/passport-naver';
+
+const naverRouter = express.Router();
+const passport = require('passport');
+
+naverRouter.get('/naver', passport.authenticate('naver'), 
+  (req:Request, res:Response) => {
+    res.writeHead(200, { 'Set-Cookie': 'session_id =req' });
+    res.send();
+  },
+);
+
+naverRouter.get('/naver/callback', passport.authenticate('naver', { failureRedirect: '/naver'}),
+  (req:Request, res:Response) => {
+    console.log('-----------req:', req.cookies)
+    res.cookie('session_id', "id")
+    res.redirect('http://localhost:3000/mypage')
+  }
+);
+
+export = naverRouter;
