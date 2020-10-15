2df6e55e4ef23e6335e8a1cdaf9cc23541738ba8
bapbodanbbang <62498196+bapbodanbbang@users.noreply.github.com>
2020-10-15 16:59:44 +0900

    reset

--- src/controller/user/signin.ts
+++ src/controller/user/signin.ts
@@ -10,23 +10,21 @@ import bcrypt from 'bcrypt';
 
 export async function signin (req:Request, res:Response) {
   
-  //소셜로그인이라면 req.body.socialLogin에 'kakao'나 'naver'기입
   const { email, password, name } = req.body;
-
-  //기존 코드
   const session = req!.session!;
-  const hash_password = await User.findOne({
-    where: {
-      email: `${email[0]}-${email[1]}`
-    }
-  })
-
+  
   //소셜로그인의 경우
+  //소셜로그인이라면 req.body.socialLogin에 'kakao'나 'naver'기입
   if(email[0] === 'kakao' || email[0] === 'naver'){
+    const socialUser = await User.findOne({
+      where: {
+        email: `${email[0]}-${email[1]}`
+      }
+    })
     //패스워드는 자신 이메일 기반으로 만듦
-    const encrypt_password_by_email = await bcrypt.hash(email[0], 10)
+    await bcrypt.hash(email[0], 10)
     .then(async (result) => {
-      if(hash_password === null){
+      if(socialUser === null){
         async function createUser() {
           await User.create({
             name: name,
@@ -38,7 +36,7 @@ export async function signin (req:Request, res:Response) {
       }
     })
     .then(async () => {
-      await bcrypt.compare(email[0], hash_password?.getDataValue('password'), (err, result) => {
+      await bcrypt.compare(email[0], socialUser?.getDataValue('password'), (err, result) => {
         if(err) {
           console.log(err);
           res.status(401).send("Unauthorized")
@@ -72,42 +70,48 @@ export async function signin (req:Request, res:Response) {
         }
       })
     })
-    //신규 사용자인 경우 회원가입을 먼저 시긴다.
-    //로그인 실행
   }
   //일반로그인의 경우
   else{
-    
-    await bcrypt.compare(password, hash_password?.getDataValue('password'), (err, result) => {
+    //입력한 비밀번호의 암호화를 씌우고, 이메일을 받아 기존 유저 있는지 확인한다.
+    const encrypted_password = await bcrypt.hash(password, 10);
+    //있으면 아이디 정보를 할당하고, 없으면 401을 보낸다.
+    const websiteUser = await User.findOne({
+      where : {
+        email : email
+      }
+    }).then(result => {
+      if(result === null){
+        res.status(401).send("Unauthorized-not-exist-user")
+      }else{
+        return result;
+      }
+    })
+    //암호화 된 비밀번호와 db의 암호를 비교해서 틀리면 401을 보내고
+    //맞으면 필요한 정보를 보낸다.
+    await bcrypt.compare(encrypted_password, websiteUser?.getDataValue('password'), (err, result) => {
       if(err) {
         console.log(err);
-        res.status(401).send("Unauthorized")
+        res.status(401).send("Unauthorized-not-correct-password")
       } else {
         // result = true;
-        (async function findUser() {
-          const user = await User.findOne({
-            where: {
-              email: email
-            }
-          })
-          if(user === null) {
-            res.status(401).send("Unauthorized")
-          } else {
-            console.log("user:", user?.getDataValue('id'))
-            console.log("session:", session)
-            session.userid = user?.getDataValue('id');
-            session.name = user?.getDataValue('name');
-            session.email = user?.getDataValue('email');
-            session.password_answer = user?.getDataValue('password_answer');
-            session.save(function () {
-              res.status(200).json({
-                "id": user?.getDataValue('id'),
-                "name": user?.getDataValue('name'),
-                "email": user?.getDataValue('email')
-              })
+        async function findUser() {
+          console.log("user:", websiteUser?.getDataValue('id'))
+          console.log("session:", session)
+          session.userid = websiteUser?.getDataValue('id');
+          session.name = websiteUser?.getDataValue('name');
+          session.email = websiteUser?.getDataValue('email');
+          session.password_answer = websiteUser?.getDataValue('password_answer');
+          session.save(function () {
+            res.setHeader('content-type', 'application/json');
+            res.status(200).json({
+              "id": websiteUser?.getDataValue('id'),
+              "name": websiteUser?.getDataValue('name'),
+              "email": websiteUser?.getDataValue('email')
             })
-          }
-        })();
+          })
+        };
+        findUser()
       }
     })
   }
